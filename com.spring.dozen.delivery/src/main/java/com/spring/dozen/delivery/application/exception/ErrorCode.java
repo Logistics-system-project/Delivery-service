1db4deb5641efb056b7e468428dc3b6192c6c179
MinJeong LEE <alswjd6807@naver.com>
2024-12-12 19:46:42 +0900

    [#1] add: exception, BaseEntity, ApiResponse 코드 추가

---
+++ com.spring.dozen.delivery/src/main/java/com/spring/dozen/delivery/application/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.spring.dozen.delivery.application.exception;
+
+import static org.springframework.http.HttpStatus.*;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+	DUPLICATED_DELIVERY_STAFF(CONFLICT, "이미 존재하는 배송 담당자입니다."),
+	UNSUPPORTED_STAFF_TYPE(NOT_FOUND, "존재하지 않는 배송 담당자 타입입니다."),
+	MISSING_HUB_ID(BAD_REQUEST, "Hub ID는 필수 입력 값입니다."),
+
+
+
+
+	;
+	private final HttpStatus httpStatus;
+	private final String message;
+}
